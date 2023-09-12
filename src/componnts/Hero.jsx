0c766ddb29d90ceb95573d10e7f21ebed7101cae
yassine <mugiwarajr0@gmail.com>
2023-09-12 23:14:37 +0100

    hero

--- src/componnts/Hero.jsx
+++ src/componnts/Hero.jsx
@@ -1,8 +1,34 @@
 import { motion } from "framer-motion";
 import btc from "../assets/hero/bitcoin.png";
 import etr from "../assets/hero/ethereum.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
 // eslint-disable-next-line react/prop-types
 function Hero({ setnavcolor }) {
+  const [response, setresponse] = useState(null);
+  const [loading, setloading] = useState(false);
+  const [err, seterr] = useState("");
+
+  const fetchdata = async () => {
+    try {
+      setloading(true);
+      const data = await axios.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false"
+      );
+      setresponse(data.data);
+    } catch (error) {
+      seterr(error);
+    } finally {
+      setloading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+  console.log(response);
+  console.log(err);
+  console.log(loading);
   return (
     <section id="hero">
       <motion.div
@@ -11,11 +37,30 @@ function Hero({ setnavcolor }) {
       >
         <div className="hero__container">
           <div className="hero__title">
-            <img src={btc} alt="" />
+            <img src={btc} alt="" className="hero__imgannim" />
             <h1>TRACK AND TRADE</h1>
-            <img src={etr} alt="" />
+            <img src={etr} alt="" className="hero__imgannim" />
           </div>
-          <h1 className="hero__h">CRYPTO CURRENCIES</h1>
+          <h1 className="navtarget">CRYPTO CURRENCIES</h1>
+          <div className="hero__api">
+            {loading ? (
+              <span className="loader"></span>
+            ) : (
+              response?.map((item) => {
+                return (
+                  <div key={item?.id} className="hero__api__box">
+                    <img src={item.image} alt="" />
+
+                    <p>
+                      {item?.name}{" "}
+                      <span>{item.market_cap_change_percentage_24h}%</span>
+                    </p>
+                    <p>$ {item.price_change_24h}</p>
+                  </div>
+                );
+              })
+            )}
+          </div>
         </div>
       </motion.div>
     </section>
